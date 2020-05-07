d2d29e0aba077594740af8ab1aeab3c31d8dfc17
Mike Delgrosso <mikedelgrosso@gmail.com>
2020-05-07 17:16:00 -0500

    Updates for user_setting_update and user_update

--- lib/zoom/actions/user.rb
+++ lib/zoom/actions/user.rb
@@ -25,9 +25,27 @@ module Zoom
         Utils.parse_response self.class.get("/users/#{params[:id]}", query: params.except(:id), headers: request_headers)
       end
 
+      # https://marketplace.zoom.us/docs/api-reference/zoom-api/users/userupdate
       def user_update(*args)
         params = Zoom::Params.new(Utils.extract_options!(args))
-        params.require(:id).permit(%i[first_name last_name type pmi timezone dept vanity_name host_key cms_user_id])
+        params.require(:id).permit(
+            :first_name,
+            :last_name,
+            :type,
+            :pmi,
+            :use_pmi,
+            :timezone,
+            :language,
+            :dept,
+            :vanity_name,
+            :host_key,
+            :cms_user_id,
+            :job_title,
+            :company,
+            :location,
+            :phone_number,
+            :phone_country
+        )
         Utils.parse_response self.class.patch("/users/#{params[:id]}", body: params.except(:id).to_json, headers: request_headers)
       end
 
