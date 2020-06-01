1784624f976b2e1a6fb64be7c6f0f2ba3a865202
Chris Dahms <zentrification@gmail.com>
2020-06-01 10:51:07 -0400

    change meeting_recording_list to query /users/:user_id/recordings

--- lib/zoom/actions/recording.rb
+++ lib/zoom/actions/recording.rb
@@ -4,12 +4,18 @@ module Zoom
   module Actions
     module Recording
 
-      #https://marketplace.zoom.us/docs/api-reference/zoom-api/cloud-recording/recordingget
-      # Note: meeting_id can be pmi or a meetingId
+      # https://marketplace.zoom.us/docs/api-reference/zoom-api/cloud-recording/recordingslist
       def meeting_recording_list(*args)
-        options = Utils.extract_options!(args)
-        Zoom::Params.new(options).require(:meeting_id)
-        Utils.parse_response self.class.get("/meetings/#{options[:meeting_id]}/recordings", headers: request_headers)
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.require(:user_id).permit(:page_size, :next_page_token, :mc, :trash, :to, :from, :trash_type)
+        Utils.parse_response self.class.get("/users/#{params[:user_id]}/recordings", query: params.except(:user_id).to_json, headers: request_headers)
+      end
+
+      # https://marketplace.zoom.us/docs/api-reference/zoom-api/cloud-recording/recordingget
+      def meeting_recording_get(*args)
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.require(:meeting_id)
+        Utils.parse_response self.class.get("/meetings/#{params[:meeting_id]}/recordings", headers: request_headers)
       end
 
       def meeting_recording_delete(*args)
--- spec/lib/zoom/actions/recording/meeting_recording_list_spec.rb
+++ spec/lib/zoom/actions/recording/meeting_recording_list_spec.rb
@@ -6,18 +6,18 @@ describe Zoom::Actions::Recording do
 
   before :all do
     @zc = zoom_client
-    @args = {meeting_id: "gkABCDEbbbbbbbkPuA=="}
+    @args = { user_id: "gkABCDEbbbbbbbkPuA==" }
   end
 
   describe '#recording_list action' do
     before :each do
       stub_request(
           :get,
-          zoom_url("/meetings/#{@args[:meeting_id]}/recordings")
+          zoom_url("/users/#{@args[:user_id]}/recordings")
       ).to_return(body: json_response('recording', 'meeting_recording_list'))
     end
 
-    it "requires a 'meeting_id' argument" do
+    it "requires a 'user_id' argument" do
       expect { @zc.meeting_recording_list }.to raise_error(Zoom::ParameterMissing)
     end
 
